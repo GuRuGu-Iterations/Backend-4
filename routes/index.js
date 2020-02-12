a4f43471e56726650b05877d418013b9b7c32376
GuRuGu <peter.k.nets@gmail.com>
2020-02-12 08:18:35 +0200

    Update test route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.route("/").get((req, res) => {
   res.status(200).json({ status: "success", message: "All is good" });
 });
 
