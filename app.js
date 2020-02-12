d2648fedc322323812f81d480f6c4984c5d51c0a
GuRuGu <peter.k.nets@gmail.com>
2020-02-12 08:06:00 +0200

    Add test route

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const mainRouter = require("./routes");
 
 // Set environment variables
-require("dotenv").config();
+require("dotenv").config({ path: "process.env" });
 
 const app = express();
 
 // Initialize middleware
 app.use(morgan("dev"));
 
+// Routes
+app.use("/api/main", mainRouter);
+
 // Run server
 const PORT = process.env.PORT || 5002;
 
-app.listen(PORT, () => console.log("Server is listening on port 5001"));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
