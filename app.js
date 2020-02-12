3d498626e1537ce434be115561c34f10699901e1
GuRuGu <peter.k.nets@gmail.com>
2020-02-12 07:58:53 +0200

    Set up logging

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 
-// Initialize environment variables
+// Set environment variables
 require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 7001;
+// Initialize middleware
+app.use(morgan("dev"));
+
+// Run server
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log("Server is listening on port 5001"));
